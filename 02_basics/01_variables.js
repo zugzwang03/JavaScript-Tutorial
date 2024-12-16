e032a3a460de640da29f98547dfe9245eb52c327
zugzwang03 <2025.cse63.shreyasarkar@gmail.com>
2024-12-16 22:48:23 +0530

    started with js

---
+++ 02_basics/01_variables.js
@@ -0,0 +1,25 @@
+var fullName = "Shreya Sarkar";
+
+// how to name variables
+
+// fullName - camelcase
+// full-name
+// full_name - snakecase
+// FullName - don't start with uppercase
+
+var courseName = "React JS Bootcamp";
+
+var isLoggedIn = true;
+
+var loggedCount = 34;
+
+var paymentMode;
+
+// console.log("loggedCount");
+// console.log(loggedCount);
+// console.log(paymentMode)
+
+paymentMode = "Credit Card";
+// console.log(paymentMode)
+
+// var if = 23;
