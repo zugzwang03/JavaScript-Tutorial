c0e91fbc9cb580ec9757728c80c35ead276edcce
zugzwang03 <2025.cse63.shreyasarkar@gmail.com>
2024-12-16 22:58:11 +0530

    learnt about variables and interpolation

---
+++ 02_basics/02_variables.js
@@ -0,0 +1,22 @@
+const uid = "abc123";
+
+var fullName = "Shreya Sarkar";
+var email = "shreya@gmail.com";
+var password = "129863";
+var confirmPassword = "129863";
+var courseCount = 0;
+var isLoggedInFromGoogle = false;
+
+// fullName = prompt("Enter your name");
+
+console.log(uid)
+console.log("Full Name is:" + fullName)
+console.log(email)
+
+console.log(`
+    With Unique ID: ${uid}
+    User is: ${fullName}
+    and their email is: ${email}
+    and uses the password: ${password}
+    
+`)
