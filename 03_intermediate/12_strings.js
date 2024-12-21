b8510f11c1531fcfc72447889b5b4cda00b3e7d6
zugzwang03 <2025.cse63.shreyasarkar@gmail.com>
2024-12-21 17:30:20 +0530

    understood strings and related operations

---
+++ 03_intermediate/12_strings.js
@@ -0,0 +1,11 @@
+var name = "shreya\'s";
+
+var fullName = `${name} Sarkar`
+
+console.log(name);
+console.log(fullName);
+
+console.log(fullName.charAt(0));
+console.log(fullName.endsWith("?"));
+console.log(fullName.includes("shreya"));
+console.log(fullName.toUpperCase());
