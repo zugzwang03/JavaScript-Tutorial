c0b540b27f35939bb734576e75b0b5ca6a85f113
zugzwang03 <2025.cse63.shreyasarkar@gmail.com>
2024-12-21 10:40:43 +0530

    learnt about iife

---
+++ 07_foundational/03_functional.js
@@ -0,0 +1,7 @@
+// functional programming
+// self-executing anonymous function
+// Immediately invoked function expression (IIFE)
+(function() {
+    console.log("Say Hello");
+    console.log("Again Say Hello");
+})();
